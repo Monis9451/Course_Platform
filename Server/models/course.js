a1d4f717a7e25ca6c671fff06cb8ba544e521c97
Monis9451 <mianmonishussain786@gmail.com>
2025-07-15 16:17:28 +0500

    Created user database schema and course schema

--- Server/models/course.js
+++ Server/models/course.js
@@ -2,7 +2,36 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const courseSchema = new Schema({
-  
+    title : {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        required: true
+    },
+
+    price: {
+        type: Float,
+        required: true
+    },
+
+    catagory: {
+        type: String,
+        required: true
+    },
+
+    thumbnail: {
+        type: String,
+        required: true
+    },
+
+    modules: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Module'
+    }],
+
 }, {
   timestamps: true
 });
--- Server/models/user.js
+++ Server/models/user.js
@@ -2,6 +2,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+
+  firebaseUid: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  email: {
+    type: String,
+    required: true
+  },
+
+  displayName: String,
+  role: {
+    type: String,
+    default: "user",
+    enum: ["user", "admin"]
+  },
   
 }, {
   timestamps: true
