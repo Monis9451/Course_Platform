6bf1ba7e83c0e9741c7c4c57f22ab4e2c6c06d30
Monis9451 <mianmonishussain786@gmail.com>
2025-07-21 17:32:59 +0500

    edited supabase file according to need

--- Server/config/supabase.js
+++ Server/config/supabase.js
@@ -6,4 +6,20 @@ const supabase = createClient(
   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
+const checkConnection = async () => {
+  try {
+    const { data, error } = await supabase.from('user').select('*').limit(1);
+    if (error) {
+        throw new Error(`Supabase connection error: ${error.message}`);
+    }
+    console.log('Supabase connected successfully');
+  }
+
+  catch (error) {
+    console.error('Error connecting to Supabase:', error.message);
+  }
+}
+
+checkConnection();
+
 module.exports = supabase;
