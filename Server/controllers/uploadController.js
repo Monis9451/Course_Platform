0d080a979fb8f986ac2edb50918628def1a181b1
Monis9451 <mianmonishussain786@gmail.com>
2025-08-05 12:06:33 +0500

    Handle the error using AppError function

--- Server/controllers/uploadController.js
+++ Server/controllers/uploadController.js
@@ -4,12 +4,12 @@ const { AppError } = require("../utils/appError.js");
 
 const uploadHandler = catchAsync(async (req, res, next) => {
   if (!req.file) {
-    return res.status(400).json({ message: "File is missing." });
+    return next(new AppError("File is missing.", 400));
   }
   const cloudinaryResponse = await uploadOnCloudinary(req.file.buffer);
 
   if (!cloudinaryResponse) {
-    return res.status(500).json({ message: "Error uploading file." });
+    return next(new AppError("Error uploading file to Cloudinary.", 500));
   }
 
   return res.status(200).json({
